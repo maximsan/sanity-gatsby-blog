d98ce445052ca47d059ac5b41944e3dbb1a3dc4d
maksim.shchutsky <maksim.shchutsky@cactussoft.biz>
2022-01-30 21:27:24 +0300

    chore: upgrade @sanity/cli, update .gitignore on root level

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   sanity: {
-    projectId:
-      process.env.GATSBY_SANITY_PROJECT_ID || "w4dg6l7o",
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "w4dg6l7o",
     dataset: process.env.GATSBY_SANITY_DATASET || "production",
   },
 };
